5a9234cb1b09304502a4ba64f7bf62b5b77a9cef
Sathiskumar2113 <sathiskumar2112000@gmail.com>
2024-07-26 09:06:20 +0530

    myown project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [count,setcount]= useState(0);
+const incremnt=()=>{
+
+  setcount(count+1);
+}
+const decremnt=()=>{
+  setcount(count-1);
+}
+const resert=()=>{
+  setcount(0);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <h1>incremnt {count}</h1>
+     <button onClick={incremnt}>incremnt</button>
+     <br></br>
+     <button onClick={decremnt}>decremnt</button>
+     <br></br>
+     <button onClick={resert}>resert</button>
     </div>
   );
 }
