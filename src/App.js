61e48957bdb59fc952d1efd0c925d2fa37cbe223
Sathiskumar2113 <sathiskumar2112000@gmail.com>
2024-07-26 10:31:17 +0530

    props using the state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
 
   let [count,setcount]= useState(0);
   
-
-  useEffect (()=>{
-    console.log ("runs the update the components is mounted and count is updated");
-    
-      
-    
-  } ,[count]);
-
-
-
 const incremnt=()=>{
 
   setcount(count+1);
 }
-const decremnt=()=>{
-  setcount(count-1);
-}
-const resert=()=>{
-  setcount(0);
-}
+
 
   return (
     <div className="App">
      
-     <h1>incremnt {count}</h1>
+     <Counter
+     count={count}
+     
+     
+     ></Counter>
      <button onClick={incremnt}>incremnt</button>
-     <br></br>
-     <button onClick={decremnt}>decremnt</button>
-     <br></br>
-     <button onClick={resert}>resert</button>
+     
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Counter(props) {
+   
+  return (
+    <div>
+        <h1> count:{props.count}</h1>
+      
+    </div>
+  )
+}
